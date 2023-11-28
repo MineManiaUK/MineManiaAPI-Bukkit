d1065d06a29b07d138d44ee655e087f3f66146a4
Smudge <68658429+smuddgge@users.noreply.github.com>
2023-11-28 23:51:47 +0000

    Created basic api implementation

---
+++ src/main/java/com/github/minemaniauk/bukkitapi/MineManiaAPI_Bukkit.java
@@ -0,0 +1,178 @@
+/*
+ * MineManiaAPI
+ * Used for interacting with the database and message broker.
+ *
+ * Copyright (C) 2023  MineManiaUK Staff
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.minemaniauk.bukkitapi;
+
+import com.github.cozyplugins.cozylibrary.CozyPlugin;
+import com.github.cozyplugins.cozylibrary.user.PlayerUser;
+import com.github.cozyplugins.cozylibrary.user.User;
+import com.github.minemaniauk.api.MineManiaAPI;
+import com.github.minemaniauk.api.MineManiaAPIContract;
+import com.github.minemaniauk.api.kerb.event.player.PlayerChatEvent;
+import com.github.minemaniauk.api.kerb.event.useraction.UserActionHasPermissionListEvent;
+import com.github.minemaniauk.api.kerb.event.useraction.UserActionIsOnlineEvent;
+import com.github.minemaniauk.api.kerb.event.useraction.UserActionIsVanishedEvent;
+import com.github.minemaniauk.api.kerb.event.useraction.UserActionMessageEvent;
+import com.github.smuddgge.squishyconfiguration.ConfigurationFactory;
+import com.github.smuddgge.squishyconfiguration.interfaces.Configuration;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+/**
+ * Represents the instance of the
+ * mine mania api for bukkit.
+ */
+public final class MineManiaAPI_Bukkit extends CozyPlugin implements MineManiaAPIContract {
+
+    private @NotNull Configuration configuration;
+    private @NotNull MineManiaAPI api;
+
+    @Override
+    public boolean enableCommandDirectory() {
+        return false;
+    }
+
+    @Override
+    public void onCozyEnable() {
+
+        // Set up the configuration file.
+        this.configuration = ConfigurationFactory.YAML
+                .create(this.getDataFolder(), "config.yml")
+                .setDefaultPath("config.yml");
+
+        // Set up the api.
+        this.api = MineManiaAPI.createAndSet(this.configuration, this);
+    }
+
+    @Override
+    public @Nullable UserActionHasPermissionListEvent onHasPermission(@NotNull UserActionHasPermissionListEvent event) {
+        Optional<Player> optionalPlayer = BukkitAdapter.getPlayer(event.getUser());
+
+        // Return with the correct permission checks unless the player is null.
+        return optionalPlayer.map(player -> event.setResult(player::hasPermission)).orElse(null);
+    }
+
+    @Override
+    public @Nullable UserActionIsOnlineEvent onIsOnline(@NotNull UserActionIsOnlineEvent event) {
+        return (UserActionIsOnlineEvent) event.set(BukkitAdapter.getPlayer(event.getUser()).isPresent());
+    }
+
+    @Override
+    public @Nullable UserActionIsVanishedEvent onIsVanished(@NotNull UserActionIsVanishedEvent event) {
+        Optional<Player> optionalPlayer = BukkitAdapter.getPlayer(event.getUser());
+        return optionalPlayer.map(
+                player -> (UserActionIsVanishedEvent) event.set(this.isVanished(player))
+                ).orElse(null);
+    }
+
+    @Override
+    public @Nullable UserActionMessageEvent onMessage(@NotNull UserActionMessageEvent event) {
+        Optional<Player> optionalPlayer = BukkitAdapter.getPlayer(event.getUser());
+        if (optionalPlayer.isEmpty()) return null;
+
+        // Send the message to the user.
+        User user = new PlayerUser(optionalPlayer.get());
+        user.sendMessage(event.getMessage());
+
+        // Complete the event.
+        return (UserActionMessageEvent) event.complete();
+    }
+
+    @Override
+    public @NotNull PlayerChatEvent onChatEvent(@NotNull PlayerChatEvent event) {
+
+        // Check if this server should not send the message.
+        if (!event.getServerWhiteList().contains(this.getClientName())) return event;
+
+        // Send all the players online the message.
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            new PlayerUser(player).sendMessage(event.getFormattedMessage());
+        }
+
+        return (PlayerChatEvent) event.complete();
+    }
+
+    /**
+     * Used to get the configuration file instance.
+     *
+     * @return The configuration file instance.
+     */
+    public @NotNull Configuration getConfiguration() {
+        return this.configuration;
+    }
+
+    /**
+     * Used to get this client name used in
+     * kerb events.
+     *
+     * @return This client name.
+     */
+    public @NotNull String getClientName() {
+        return this.api.getKerbClient().getName();
+    }
+
+    /**
+     * Used to get the instance of the api connection.
+     *
+     * @return The instance of the api connection.
+     */
+    public @NotNull MineManiaAPI getAPI() {
+        return this.api;
+    }
+
+    /**
+     * Used to check if a player is vanished.
+     *
+     * @param player The instance of a player.
+     * @return True if they are vanished.
+     */
+    public boolean isVanished(@NotNull Player player) {
+        for (MetadataValue meta : player.getMetadata("vanished")) {
+            if (meta.asBoolean()) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Used to get the instance of the bukkit api.
+     * This can be called from any other plugin.
+     * <ul>
+     *     <li>Uses {@link Bukkit} to get the instance of the plugin</li>
+     * </ul>
+     *
+     * @return The instance of the bukkit api.
+     */
+    public static @NotNull MineManiaAPI_Bukkit getInstance() {
+        Plugin plugin = Bukkit.getPluginManager().getPlugin("MineManiaAPI-Bukkit");
+
+        // Check if it doesn't exist.
+        if (plugin == null) {
+            throw new RuntimeException("Could not get the instance of the mine mania api bukkit.");
+        }
+
+        return (MineManiaAPI_Bukkit) plugin;
+    }
+}
---
+++ src/main/java/com/github/minemaniauk/bukkitapi/listener/PlayerChatListener.java
@@ -0,0 +1,83 @@
+/*
+ * MineManiaAPI
+ * Used for interacting with the database and message broker.
+ *
+ * Copyright (C) 2023  MineManiaUK Staff
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.minemaniauk.bukkitapi.listener;
+
+import com.github.kerbity.kerb.result.CompletableResultSet;
+import com.github.minemaniauk.api.format.ChatFormat;
+import com.github.minemaniauk.api.kerb.event.player.PlayerChatEvent;
+import com.github.minemaniauk.api.kerb.event.player.PlayerPostChatEvent;
+import com.github.minemaniauk.bukkitapi.BukkitAdapter;
+import com.github.minemaniauk.bukkitapi.MineManiaAPI_Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents the player chat listener.
+ * Used to listen for player messages and send them to kerb.
+ */
+public class PlayerChatListener implements Listener {
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        // Stop players from seeing the message.
+        try {
+            event.getRecipients().clear();
+        } catch (UnsupportedOperationException ignored) {
+            event.setCancelled(true);
+        }
+
+        // Call the post-chat event.
+        CompletableResultSet<PlayerPostChatEvent> completableResultSet = MineManiaAPI_Bukkit
+                .getInstance()
+                .getAPI()
+                .callEvent(new PlayerPostChatEvent(
+                        BukkitAdapter.getUser(event.getPlayer()),
+                        event.getMessage()
+                ));
+
+        // Wait for the final result set.
+        List<PlayerPostChatEvent> resultSet = completableResultSet.waitForFinalResult();
+
+        // Check if the event was cancelled.
+        if (completableResultSet.containsCancelled()) return;
+
+        // The final chat format.
+        ChatFormat chatFormat = new ChatFormat();
+        List<String> serverWhiteList = new ArrayList<>();
+
+        for (PlayerPostChatEvent postChatEvent : resultSet) {
+            if (postChatEvent == null) continue;
+            chatFormat.combine(postChatEvent.getChatFormat());
+            serverWhiteList.addAll(postChatEvent.getServerWhitelist());
+        }
+
+        // Broadcast the final chat event and not expect results.
+        MineManiaAPI_Bukkit.getInstance().getAPI().callEvent(new PlayerChatEvent(
+                BukkitAdapter.getUser(event.getPlayer()),
+                chatFormat.parse(event.getMessage()),
+                serverWhiteList
+        ));
+    }
+}
