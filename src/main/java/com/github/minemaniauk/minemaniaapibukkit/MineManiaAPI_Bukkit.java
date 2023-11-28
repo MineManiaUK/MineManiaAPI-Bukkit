61758bd704b4f6544b35ff0385572bb7f44b4445
Smudge <68658429+smuddgge@users.noreply.github.com>
2023-11-28 18:37:36 +0000

    Basic project structure

---
+++ src/main/java/com/github/minemaniauk/minemaniaapibukkit/MineManiaAPI_Bukkit.java
@@ -0,0 +1,17 @@
+package com.github.minemaniauk.minemaniaapibukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MineManiaAPI_Bukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
