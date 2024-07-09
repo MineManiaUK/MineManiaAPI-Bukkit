d43a274f7752ae72af717e6dc11bccf45b80c5fe
Smudge <68658429+smuddgge@users.noreply.github.com>
2024-07-09 23:11:49 +0100

    Update MineManiaAPI_Bukkit.java

--- src/main/java/com/github/minemaniauk/bukkitapi/MineManiaAPI_Bukkit.java
+++ src/main/java/com/github/minemaniauk/bukkitapi/MineManiaAPI_Bukkit.java
@@ -87,7 +87,6 @@ public final class MineManiaAPI_Bukkit extends CozyPlugin implements MineManiaAP
 
         // Set up the api.
         this.api = MineManiaAPI.createAndSet(this.configuration, this);
-        this.api.getKerbClient().setDebugMode(true);
 
         // Set up the teleport list.
         this.teleportMap = new HashMap<>();
